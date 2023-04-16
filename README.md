a1b90bd7f3ac32471e592d04b75760beeb86b6c7
tritium-vlk <11885129+Tritium-VLK@users.noreply.github.com>
2023-04-16 19:01:18 +0200

    push deployment script and read me with addresses.

---
+++ README.md
@@ -0,0 +1,3 @@
+## Deployed addresses
+https://polygonscan.com/address/0xa289a03f46f144fAaDd9Fc51b006d7322ECc9B04#writeContract
+https://arbiscan.io/address/0xa289a03f46f144fAaDd9Fc51b006d7322ECc9B04
---
+++ scripts/deploy.py
@@ -0,0 +1,28 @@
+import requests
+from brownie import chain, PoolCreationHelper, accounts
+
+account = accounts.load("dd_PoolCreation")
+BALANCER_DEPLOYMENTS_URL = "https://raw.githubusercontent.com/balancer-labs/balancer-v2-monorepo/master/pkg/deployments"
+CHAIN_NAME_BY_ID = {
+        1: "mainnet",
+        137: "polygon",
+        42161: "arbitrum",
+        10: "optimism",
+        100: "gnosis",
+        42: "goerli",
+}
+
+def monorepoLookup(task, contract):
+    chainname = CHAIN_NAME_BY_ID[chain.id]
+    deployment_output = requests.get( f"{BALANCER_DEPLOYMENTS_URL}/tasks/{task}/output/{chainname}.json").json()
+    target_address = deployment_output[contract]
+    return target_address
+
+vault = monorepoLookup("20210418-vault", "Vault")
+weightedFactory = monorepoLookup("20230320-weighted-pool-v4", "WeightedPoolFactory")
+stableFactory = monorepoLookup("20230320-composable-stable-pool-v4", "ComposableStablePoolFactory")
+
+helper = PoolCreationHelper.deploy(vault, weightedFactory, stableFactory, {"from": account})
+PoolCreationHelper.publish_source(PoolCreationHelper[0])
+
+
